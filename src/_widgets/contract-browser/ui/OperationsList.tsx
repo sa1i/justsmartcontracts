01fb9d7d5c1f36a893fdf840cb06100482b218e5
sa1i <sai@dodoex.io>
2024-06-05 03:36:11 +0800

    support function selector

--- src/_widgets/contract-browser/ui/OperationsList.tsx
+++ src/_widgets/contract-browser/ui/OperationsList.tsx
@@ -1,6 +1,7 @@
 import { Collapse } from "antd";
 import { TContract, contractModel } from "@entities/contract";
 import { CreateTransaction } from "@features/sign-transaction";
+import { getFunctionSelector } from "viem";
 
 type TProps = {
   contract: TContract;
@@ -9,11 +10,14 @@ type TProps = {
 export const OperationsList = ({ contract }: TProps) => {
   const functions = contractModel.useContractOperations(contract);
 
-  const items = functions.map((item, index) => ({
-    label: item.name,
-    key: index,
-    children: <CreateTransaction contract={contract} abiItem={item} />,
-  }));
+  const items = functions.map((item, index) => {
+    const hashFunction = getFunctionSelector(item);
+    return {
+      label: `${item.name}  (${hashFunction})`,
+      key: index,
+      children: <CreateTransaction contract={contract} abiItem={item} />,
+    };
+  });
 
   return <Collapse items={items} />;
 };
